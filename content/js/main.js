5dabe9cbe6cdf042bc61a0f039e2951258fe1dd8
Zeigfreid <various.authors@gmail.com>
2015-05-22 16:02:55 -0700

    convert global mouse event click into local position... now we can click on stuff in the light!

--- content/js/main.js
+++ content/js/main.js
@@ -1,29 +1,15 @@
-var bunny;
 var WIDTH = 800;
 var HEIGHT = 600;
 var RADIUS = Math.sqrt(Math.pow(WIDTH/2, 2) + Math.pow(HEIGHT/2, 2)) + Math.max(WIDTH, HEIGHT)/5;
+var light, triangle;
 
-function initBunny (stage) {
-    // create a texture from an image path
-    var texture = PIXI.Texture.fromImage('images/bunny.png');
+function onDown (e) {
+    var target = e.target;
+    var point = e.data.getLocalPosition(target);
 
-    // create a new Sprite using the texture
-    var bunny = new PIXI.Sprite(texture);
+    if (! light.containsPoint(point)) { return false; }
 
-    // center the sprite's anchor point
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
-
-    // move the sprite to the center of the screen
-    bunny.position.x = 200;
-    bunny.position.y = 150;
-
-    return bunny;
-}
-
-function updateBunny () {
-    // just for fun, let's rotate mr rabbit a little
-    bunny.rotation += 0.1;
+    console.log("CLICKY");
 }
 
 (function () {
@@ -34,24 +20,40 @@ function updateBunny () {
     var stage = new PIXI.Container();
 
     stage.interactive = true;
+    stage.on('mousedown', onDown);
 
-    var graphics = new PIXI.Graphics();
+    var background = new PIXI.Graphics();
 
     // set a fill and a line style again and draw a rectangle
-    graphics.lineStyle(2, 0x0000FF, 1);
-    graphics.beginFill(0x1099bb, 1);
-    graphics.drawRect(0, 0, WIDTH, HEIGHT);
+    background.lineStyle(2, 0x0000FF, 1);
+    background.beginFill(0x1099bb, 1);
+    background.drawRect(0, 0, WIDTH, HEIGHT);
+
+    stage.addChild(background);
+
+    // make a beam of light
+    light = new PIXI.Graphics();
+    light.position.x = WIDTH/2 + WIDTH/4;
+    light.position.y = HEIGHT/2 + HEIGHT/4;
+    stage.addChild(light);
+
+    triangle = new PIXI.Graphics();
 
     // set a fill and line style
-    graphics.beginFill(0xFF3300);
-    graphics.lineStyle(4, 0xffd900, 1);
+    triangle.beginFill(0xFF3300);
+    triangle.lineStyle(4, 0xffd900, 1);
 
     // draw a shape
-    graphics.moveTo(50,50);
-    graphics.lineTo(250, 50);
-    graphics.lineTo(100, 100);
-    graphics.lineTo(50, 50);
-    graphics.endFill();
+    triangle.moveTo(50,50);
+    triangle.lineTo(250, 50);
+    triangle.lineTo(100, 100);
+    triangle.lineTo(50, 50);
+    triangle.endFill();
+    triangle.interactive = true;
+
+    stage.addChild(triangle);
+
+    var graphics = new PIXI.Graphics();
 
     // draw a rounded rectangle
     graphics.lineStyle(2, 0xFF00FF, 1);
@@ -65,12 +67,7 @@ function updateBunny () {
     graphics.drawCircle(470, 90,60);
     graphics.endFill();
 
-    // make a beam of light
-    var light = new PIXI.Graphics();
-    stage.addChild(light);
-    light.position.x = WIDTH/2 + WIDTH/4;
-    light.position.y = HEIGHT/2 + HEIGHT/4;
-
+    // draw a thing?
     graphics.moveTo(50,50);
     graphics.lineTo(250, 50);
     graphics.lineTo(100, 100);
@@ -96,17 +93,21 @@ function updateBunny () {
 
         light.clear();
 
-        light.beginFill(0x8bc5ff, 0.4);
+        light.beginFill(0xFFFFFF, 0.4);
         light.moveTo(0, 0);
         light.lineTo(cx_1, cy_1);
         light.lineTo(cx_2, cy_2);
         light.lineTo(0, 0);
-
-        //light.rotation = count * 0.1;
+        light.endFill();
+        light.hitArea = light.getBounds();
 
         renderer.render(stage);
         requestAnimationFrame( animate );
     }
+
+    // when the light leaves one half of the screen, the objects on that half get shuffled about
+
+    // when the player clicks on an object, if it is visible, they get the museum entry
 
     // run the render loop
     animate();
